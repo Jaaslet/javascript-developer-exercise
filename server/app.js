777dab9350ea38d4daee41950c1ace21a5d49fd0
Jonas Aaslet <jonas_bms@hotmail.com>
2016-07-17 21:09:02 +0200

    the server can now fetch blogposts

--- server/app.js
+++ server/app.js
@@ -1,11 +1,29 @@
-var express = require('express');
+var express = require("express");
+var request = require("request");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  request({
+    uri: "https://jsonblob.com/api/jsonBlob/578bd07be4b0dc55a4e539a9",
+    method: "GET",
+    timeout: 10000,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  }, function(error, response, body) {
+    json=JSON.parse(body);
+    res.send(json.posts);
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+
+
+function auth(user,pass) //make sure the user is logged in
+{
+  return user=="test" && pass=="6655321";
+}

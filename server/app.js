8b68def52ad0f21d370138ad3fbc18ddcd5de706
Jonas Aaslet <jonas_bms@hotmail.com>
2016-07-18 13:57:18 +0200

    sever can almost add new blogposts and edit old

--- server/app.js
+++ server/app.js
@@ -24,19 +24,38 @@ app.get('/', function (req, res, next) //returns the blogposts as JSON
       "Accept": "application/json"
     }
   }, function(error, response, body) {
-    json=JSON.parse(body);
+    var json = JSON.parse(body);
     res.json(JSON.stringify(json.posts));
   });
 });
 
 app.post('/login', function (req, res, next)
 {
-    if(auth(req.param.user,req.param.pass)) res.send("success");
-    else res.send("Wrong username or password");
+  if(auth(req.param.user,req.param.pass)) res.send("success");
+  else res.send("Wrong username or password");
+});
+
+app.post('/add', function (req, res, next) //edits and creates blogposts
+{
+  if(!auth(req.param.user,req.param.pass)) return;
+  var json = "{\"posts\":[\"This blogpost is a test\",\"This is also a test\",\"TEST3\"]}"; //TODO: make this work: req.param.json;
+  request({
+    uri: "https://jsonblob.com/api/jsonBlob/578bd07be4b0dc55a4e539a9",
+    method: "PUT",
+    json: true,
+    timeout: 10000,
+    body: json,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+    }, function(error, response, body) {
+      res.send(error);
+    });
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Blog app listening on port 3000!');
 });
 
 
